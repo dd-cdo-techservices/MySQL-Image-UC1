d8093fecc15454007f74c13aa2d20f5cdf449d3e
yogisdm <31575108+yogisdm@users.noreply.github.com>
2019-05-28 13:28:01 +0530

    code fix

--- mysql.sh
+++ mysql.sh
@@ -13,7 +13,9 @@ echo "Installing MySQL 5.7 now...\n"
 sudo echo "mysql-server-5.7 mysql-server/root_password password Password@123" | sudo debconf-set-selections
 sudo echo "mysql-server-5.7 mysql-server/root_password_again password Password@123" | sudo debconf-set-selections
 
-if sudo apt-get -y install mysql-server-5.7 mysql-client
+
+
+if sudo apt-get -y install mysql-client
 then
 echo "Clean installation of mysql, no broken packages"
 else
@@ -21,6 +23,14 @@ else
  echo "MySQL broken packages fix"
 fi
 
+
+if sudo apt-get -y install mysql-server-5.7
+then
+echo "Clean installation of mysql, no broken packages"
+else
+ sudo apt-get -y install -f
+ echo "MySQL broken packages fix"
+fi
 
 #mysql -u root -p root -e "use mysql; UPDATE user SET authentication_string=PASSWORD('$MYSQLPASSWORD') WHERE User='root'; flush privileges;" >> $LOGFILE 2>&1
 
