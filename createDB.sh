bdbc4f11bcd7d6b8de8b48a0677dba6d808c8d66
Vinay Kuamr Mirle <vinay.mbkumar@gmail.com>
2019-08-05 16:10:58 +0530

    Update createDB.sh

--- createDB.sh
+++ createDB.sh
@@ -4,7 +4,7 @@ sudo mysqladmin -u root -pPassword@123 create jwt
 
 mysql -u root -D mysql -pPassword@123 -e "use mysql;UPDATE user SET Host='%' WHERE Host='localhost' AND User='root';"
 
-mysql -u root -pPassword@123 -e "use jwt;CREATE table USER (first_name varchar(20), last_name varchar(20), email varchar(20), username varchar(20), password varchar(20), regdate varchar(20));"
+mysql -u root -pPassword@123 -e "use jwt;CREATE table USER (first_name varchar(200), last_name varchar(200), email varchar(200), username varchar(20), password varchar(20), regdate varchar(20));"
 
 sudo systemctl restart mysql
 
